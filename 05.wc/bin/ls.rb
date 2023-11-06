d0eb8bef972a354fd486b01d9646085ad7be6eb7
shunhamm <shun.022885@gmail.com>
2023-11-06 17:19:46 +0900

    最新のmainブランチをマージした結果を反映

--- 05.wc/bin/ls.rb
+++ 05.wc/bin/ls.rb
@@ -8,14 +8,17 @@ def main
   option = {}
   opt = OptionParser.new
 
+  opt.on('-a') { |v| option[:a] = v }
+  opt.on('-r') { |v| option[:r] = v }
   opt.on('-l') { |v| option[:l] = v }
   opt.parse!(ARGV)
   path = ARGV[0].nil? ? '.' : ARGV[0]
   files = files(path)
-  transformed_files = transform_files_for_long_format(files, option, path)
+  transformed_files = transform_by_option(files, option, path)
 
   if option[:l]
-    transformed_files.each { |files| puts files }
+    puts "total #{transformed_files[0]}"
+    transformed_files[1..].each { |files| puts files }
   else
     show_as_grid(transformed_files)
   end
