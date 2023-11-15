ce5146fec43ff030f954210bcd355bae5bc33eb0
shunhamm <shun.022885@gmail.com>
2023-11-15 11:01:04 +0900

    mapを使ったリファクタが漏れていた箇所を修正

--- 05.wc/lib/lib_wc.rb
+++ 05.wc/lib/lib_wc.rb
@@ -8,14 +8,13 @@ def run_wc(file_names, option)
 end
 
 def wc(file_names, option)
-  wc = []
   total_file_data = { lines: 0, words: 0, bytes: 0 }
 
-  file_names.each do |file|
+  wc = file_names.map do |file|
     file_data = build_file_data(File.new(file).read)
     %i[lines words bytes].each { |key| total_file_data[key] += file_data[key] }
     body = render_body(file_data, option)
-    wc << [body, file].join(' ')
+    [body, file].join(' ')
   end
   if file_names.size > 1
     total = "#{render_body(total_file_data, option)} total"
