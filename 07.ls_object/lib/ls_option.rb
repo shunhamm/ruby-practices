29011429826fd43c492655262c314ee5f613ccc0
shunhamm <shun.022885@gmail.com>
2023-11-17 15:38:43 +0900

    ls_optionを実装

--- 07.ls_object/lib/ls_option.rb
+++ 07.ls_object/lib/ls_option.rb
@@ -1,4 +1,24 @@
 # frozen_string_literal: true
 
 class LsOption
+  def initialize(args = ARGV)
+    @options = {}
+    parse(args)
+  end
+
+  def option_set?(option)
+    !@options[option].nil?
+  end
+
+  private
+
+  def parse(args)
+    OptionParser.new do |opts|
+      opts.on('-l') { @options['-l'] = true }
+      opts.on('-a') { @options['-a'] = true }
+      opts.on('-r') { @options['-r'] = true }
+    end.parse!(args)
+  rescue OptionParser::InvalidOption => e
+    raise e, '適切なオプションを指定して下さい。'
+  end
 end
