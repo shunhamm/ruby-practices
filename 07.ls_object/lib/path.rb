7b4cfe61905e9032cf4fec24a68bf4089b51fd04
shunhamm <shun.022885@gmail.com>
2023-11-17 12:21:44 +0900

    Pathクラスの作成

--- 07.ls_object/lib/path.rb
+++ 07.ls_object/lib/path.rb
@@ -1,4 +1,13 @@
 # frozen_string_literal: true
 
 class Path
+  def initialize(argv = nil)
+    @path = argv || '.'
+  end
+
+  def files
+    Dir.entries(@path).sort
+  rescue Errno::ENOENT => e
+    raise e, "Path not found: #{@path}"
+  end
 end
--- 07.ls_object/spec/path_spec.rb
+++ 07.ls_object/spec/path_spec.rb
@@ -7,8 +7,15 @@ describe Path do
   describe '#files' do
     context 'when path is available' do
       it 'returns the files' do
-        path = Path.new
-        expect(path.files).to eq(['dummy', 'dummy2.txt', 'dummy_1.txt', 'dummy_dir'])
+        path = Path.new('./spec/fixtures/dummy_files')
+        expect(path.files).to eq(['.', '..', 'dummy', 'dummy2.txt', 'dummy_1.txt', 'dummy_dir'])
+      end
+    end
+
+    context 'when path is not available' do
+      it 'raises an error' do
+        path = Path.new('./spec/fixtures/dammy_files')
+        expect { path.files }.to raise_error(Errno::ENOENT)
       end
     end
   end
