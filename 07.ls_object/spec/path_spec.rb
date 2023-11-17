286e0b3668e2484a29d4c85b42891fb711e0066b
shunhamm <shun.022885@gmail.com>
2023-11-17 10:22:06 +0900

    Pathのテストを作成

--- 07.ls_object/spec/path_spec.rb
+++ 07.ls_object/spec/path_spec.rb
@@ -3,6 +3,13 @@
 require 'spec_helper'
 require_relative '../lib/path'
 
-describe FilePath do
-  describe
+describe Path do
+  describe '#files' do
+    context 'when path is available' do
+      it 'returns the files' do
+        path = Path.new
+        expect(path.files).to eq(['dummy', 'dummy2.txt', 'dummy_1.txt', 'dummy_dir'])
+      end
+    end
+  end
 end
