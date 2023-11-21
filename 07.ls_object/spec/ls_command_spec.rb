1362ea4fb897dd5ad91490664c70668b8e8f6ec0
shunhamm <shun.022885@gmail.com>
2023-11-21 14:32:40 +0900

    テスト内容を修正

--- 07.ls_object/spec/ls_command_spec.rb
+++ 07.ls_object/spec/ls_command_spec.rb
@@ -3,43 +3,52 @@
 require 'spec_helper'
 require_relative '../lib/ls_command'
 require_relative '../lib/ls_option'
-require_relative '../lib/path'
+require_relative '../lib/file_directory'
 
-describe LsCommand do # rubocop:disable all
-  describe '#show_ls' do # rubocop:disable all
+describe LsCommand do
+  describe '#run_ls' do
     context 'when a option is given' do
       before do
-        argv = ['-a', '07.ls_object/spec/fixtures/dummy_files']
+        argv = ['-a', 'spec/fixtures/dummy_files']
         @ls_commnad = LsCommand.new(argv)
       end
+
       it 'has files including hidden files' do
-        expect(@ls_commnad.show_ls).to be '.               ..              dummy           dummy2.txt      dummy_1.txt     dummy_dir'
+        expect { @ls_commnad.run_ls }.to output(<<~HEREDOC).to_stdout
+          .             dummy         dummy_1.txt#{'   '}
+          ..            dummy2.txt    dummy_dir#{'     '}
+        HEREDOC
       end
     end
 
     context 'when r option is given' do
       before do
-        argv = ['-a', '07.ls_object/spec/fixtures/dummy_files']
+        argv = ['-r', 'spec/fixtures/dummy_files']
         @ls_commnad = LsCommand.new(argv)
       end
+
       it 'has files including hidden files' do
-        expect(@ls_commnad.show_ls).to be 'dummy_dir       dummy_1.txt     dummy2.txt      dummy'
+        expect { @ls_commnad.run_ls }.to output(<<~HEREDOC).to_stdout
+          dummy_dir     dummy2.txt#{'    '}
+          dummy_1.txt   dummy#{'         '}
+        HEREDOC
       end
     end
 
     context 'when l option is given' do
       before do
-        argv = ['-a', '07.ls_object/spec/fixtures/dummy_files']
+        argv = ['-l', 'spec/fixtures/dummy_files']
         @ls_commnad = LsCommand.new(argv)
       end
+
       it 'has files including hidden files' do
-        expect(@ls_commnad.show_ls).to be '
-        total 0
--rw-r--r--@ 1 shunhamm  staff   0 11 17 10:18 dummy
--rw-r--r--@ 1 shunhamm  staff   0 11 17 10:18 dummy2.txt
--rw-r--r--@ 1 shunhamm  staff   0 11 17 10:18 dummy_1.txt
-drwxr-xr-x@ 2 shunhamm  staff  64 11 17 10:18 dummy_dir
-        '
+        expect { @ls_commnad.run_ls }.to output(<<~HEREDOC).to_stdout
+          total 0
+          -rw-r--r-- 1 shunhamm staff   0 11 17 10:18 dummy
+          -rw-r--r-- 1 shunhamm staff   0 11 17 10:18 dummy2.txt
+          -rw-r--r-- 1 shunhamm staff   0 11 17 10:18 dummy_1.txt
+          drwxr-xr-x 2 shunhamm staff  64 11 17 10:18 dummy_dir
+        HEREDOC
       end
     end
   end
