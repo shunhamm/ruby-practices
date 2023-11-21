5e449e644cc702081b5e4af8956098a30b5c20e6
shunhamm <shun.022885@gmail.com>
2023-11-21 15:40:49 +0900

    命名の変更

--- 07.ls_object/spec/file_directory_spec.rb
+++ 07.ls_object/spec/file_directory_spec.rb
@@ -1,20 +1,20 @@
 # frozen_string_literal: true
 
 require 'spec_helper'
-require_relative '../lib/path'
+require_relative '../lib/file_directory'
 
-describe Path do
+describe FileDirectory do
   describe '#files' do
     context 'when path is available' do
       it 'returns the files' do
-        path = Path.new('./spec/fixtures/dummy_files')
+        path = FileDirectory.new('./spec/fixtures/dummy_files')
         expect(path.files).to eq(['.', '..', 'dummy', 'dummy2.txt', 'dummy_1.txt', 'dummy_dir'])
       end
     end
 
     context 'when path is not available' do
       it 'raises an error' do
-        path = Path.new('./spec/fixtures/dammy_files')
+        path = FileDirectory.new('./spec/fixtures/dammy_files')
         expect { path.files }.to raise_error(Errno::ENOENT)
       end
     end
