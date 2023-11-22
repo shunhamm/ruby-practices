8f50b9b9d88d3f6f146d71078d31811b6fe6f1b2
shunhamm <shun.022885@gmail.com>
2023-11-22 11:22:55 +0900

    rubocopの修正

--- 07.ls_object/lib/file_meta_data.rb
+++ 07.ls_object/lib/file_meta_data.rb
@@ -3,13 +3,13 @@
 class FileMetaData
   attr_reader :file_type, :permissions, :links, :owner, :group, :size, :last_modified
 
-  def initialize(file_type:, permissions:, links:, owner:, group:, size:, last_modified:)
-    @file_type = file_type
-    @permissions = permissions
-    @links = links
-    @owner = owner
-    @group = group
-    @size = size
-    @last_modified = last_modified
+  def initialize(attributes)
+    @file_type = attributes[:file_type]
+    @permissions = attributes[:permissions]
+    @links = attributes[:links]
+    @owner = attributes[:owner]
+    @group = attributes[:group]
+    @size = attributes[:size]
+    @last_modified = attributes[:last_modified]
   end
 end
